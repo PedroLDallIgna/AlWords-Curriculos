062db9cbaf54246c62eb68ed24d413de9399922c
PedroLDallIgna <pedro.lucas-di@hotmail.com>
2021-09-25 14:39:06 -0300

    09-25 forms signup and signin ready

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, RadioField, SubmitField, SelectField
 from wtforms.fields.html5 import EmailField, TelField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 class SignUp(FlaskForm):
     name = StringField(label='Nome', validators=[DataRequired()])
     last_name = StringField(label="Sobrenome", validators=[DataRequired()])
-    email = EmailField(label="Email", validators=[DataRequired()])
-    password = PasswordField(label="Senha", validators=[DataRequired()])
+    email = EmailField(label="Email", validators=[DataRequired(), EqualTo('confirm_email')])
+    confirm_email = EmailField(label="Confirme o Email", validators=[DataRequired()])
+    password = PasswordField(label="Senha", validators=[DataRequired(), EqualTo('confirm_password')])
+    confirm_password = PasswordField(label="Confirme a Senha", validators=[DataRequired()])
     sex = SelectField(label="Sexo", choices=[('M', 'Masculino'), ('F', 'Feminino')], validators=[DataRequired()])
     # sex = RadioField(label="Sexo", choices=[('M', 'Male'), ('F', 'Female')], validators=[DataRequired()])
     phone = TelField(label="Telefone", validators=[DataRequired()])
